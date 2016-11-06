b1ad27bc8e06e390bfd0cc849afd046f8faedf5c
Pai Peng <p.peng@s2icode.com>
2016-11-06 15:00:55 +0800

    Initial Commit

---
+++ GradienterView/GradienterView.h
@@ -0,0 +1,19 @@
+//
+//  GradienterView.h
+//  GradienterView
+//
+//  Created by Pai Peng on 06/11/2016.
+//  Copyright © 2016 Pai Peng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GradienterView.
+FOUNDATION_EXPORT double GradienterViewVersionNumber;
+
+//! Project version string for GradienterView.
+FOUNDATION_EXPORT const unsigned char GradienterViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GradienterView/PublicHeader.h>
+
+
